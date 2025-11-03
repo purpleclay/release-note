cbd3d5af638d21cb97a96bb3df8d80f004095658
Purple Clay <purpleclaygh@gmail.com>
2025-11-03 06:18:34 +0000

    feat: extend the built-in release note template to include the wrapped commit body (#46)
    Closes #42
    
    The body of a commit will now be included within the release note by
    default. It will automatically wrap at 80 characters to ensure output is
    compliant with most editors. Wrapping is typically ignored by markdown
    renderers (GitHub and GitLab), and serves only to prettify the output.

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,10 +1,16 @@
 use crate::analyzer::{CategorizedCommits, CommitCategory};
 use anyhow::{Context, Result};
+use std::collections::HashMap;
+use tera::Value;
 
 pub const TEMPLATE: &str = r#"{%- if breaking %}
 ## Breaking Changes
 {%- for commit in breaking %}
 - {{ commit.hash }} {{ commit.first_line }}
+{%- if commit.body %}
+
+{{ commit.body | wrap(width=80) | indent(prefix = "  ", first=true) }}
+{%- endif %}
 {%- endfor %}
 
 {%- endif %}
@@ -12,6 +18,10 @@ pub const TEMPLATE: &str = r#"{%- if breaking %}
 ## New Features
 {%- for commit in features %}
 - {{ commit.hash }} {{ commit.first_line }}
+{%- if commit.body %}
+
+{{ commit.body | wrap | indent(prefix = "  ", first=true) }}
+{%- endif %}
 {%- endfor %}
 
 {%- endif %}
@@ -19,6 +29,10 @@ pub const TEMPLATE: &str = r#"{%- if breaking %}
 ## Bug Fixes
 {%- for commit in fixes %}
 - {{ commit.hash }} {{ commit.first_line }}
+{%- if commit.body %}
+
+{{ commit.body | wrap | indent(prefix = "  ", first=true) }}
+{%- endif %}
 {%- endfor %}
 
 {%- endif %}
@@ -26,16 +40,36 @@ pub const TEMPLATE: &str = r#"{%- if breaking %}
 ## Dependency Updates
 {%- for commit in dependencies %}
 - {{ commit.hash }} {{ commit.first_line }}
+{%- if commit.body %}
+
+{{ commit.body | wrap | indent(prefix = "  ", first=true) }}
+{%- endif %}
 {%- endfor %}
 
 {%- endif %}
 
 *Generated with [release-note](https://github.com/purpleclay/release-note)*"#;
 
+fn wrap_filter(value: &Value, args: &HashMap<String, Value>) -> tera::Result<Value> {
+    let text = value
+        .as_str()
+        .ok_or_else(|| tera::Error::msg("wrap filter requires a string value"))?;
+
+    let width = args.get("width").and_then(|v| v.as_u64()).unwrap_or(80) as usize;
+
+    let (unwrapped, _) = textwrap::unfill(text);
+
+    let options = textwrap::Options::new(width);
+    let wrapped = textwrap::fill(&unwrapped, options);
+    Ok(Value::String(wrapped))
+}
+
 pub fn render_history(categorized: &CategorizedCommits) -> Result<String> {
     let mut tera = tera::Tera::default();
     tera.add_raw_template("main", TEMPLATE)
         .context("failed to parse template")?;
+
+    tera.register_filter("wrap", wrap_filter);
 
     let mut context = tera::Context::new();
 
